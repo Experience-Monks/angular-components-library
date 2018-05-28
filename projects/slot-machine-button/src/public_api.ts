5f1203979cfb656b6a4641cc237c8f833af730f4
Neo <neo.li@jam3.com>
2018-05-28 14:35:39 -0400

    Adjust the name and remove unnecessary files

--- projects/slot-machine-button/src/public_api.ts
+++ projects/slot-machine-button/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of slot-machine-button
  */
 
-export * from './lib/slot-machine-button.service';
 export * from './lib/slot-machine-button.component';
 export * from './lib/slot-machine-button.module';
